424e992e175cc0b0d1d867ddff8630940d70ae7a
Sagar developer <sagarp.developer@webzlabz.com>
2021-08-10 12:58:40 +0530

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,6 +26,12 @@ namespace DemoGitnormal.Controllers
 
             return View();
         }
-        
+        public ActionResult Contact1()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
     }
 }
